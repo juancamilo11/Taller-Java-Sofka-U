182b812eb4dca911855ff00192f87eaf75748402
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-07 09:33:44 -0500

    6th activity finished

--- src/main/java/dev/j3c/practice6/MainClass.java
+++ src/main/java/dev/j3c/practice6/MainClass.java
@@ -1,19 +1,14 @@
-package dev.j3c.practice5;
-
-import javax.swing.*;
-import java.util.InputMismatchException;
+package dev.j3c.practice6;
 
 public class MainClass {
-
+    
     public static void main(String[] args) {
-        int num = 2;
         System.out.println("The list of the odd numbers from 1 to 100 (both included) is: \n");
         System.out.print("1" + " ");
-        while (num < 100) {
-            if(num %2 != 0) {
+        for (int num = 3 ; num < 100 ; num++) {
+            if(num%2 != 0) {
                 System.out.print(num + " ");
             }
-            num++;
         }
         System.out.print("100" + " ");
     }
