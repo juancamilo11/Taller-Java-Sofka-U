3a7814a3fa9054d3e55889fabd562570eefd01cd
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-07 09:21:37 -0500

    4th activity finished

---
+++ src/main/java/dev/j3c/practice4/MainClass.java
@@ -0,0 +1,35 @@
+package dev.j3c.practice4;
+
+import javax.swing.*;
+import java.util.InputMismatchException;
+
+public class MainClass {
+
+    private static double inputPrice() {
+        Double price = null;   //Wrapper class (Autoboxing operation)
+        do {
+            try {
+                price = Double.parseDouble(JOptionPane.showInputDialog("Enter the price of an item calculating the final price by adding its IVA: "));
+            } catch (NumberFormatException e1) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e1.getMessage());
+            } catch(InputMismatchException e2) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e2.getMessage());
+            }
+        } while(price == null || price < 0);
+        return(price);
+    }
+
+    private static double calculateFinalPrice(double price) {
+        return (price + (price * 0.21));
+    }
+//4. Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
+//calcule el precio final con IVA. El IVA sera una constante que sera del 21%.
+    public static void main(String[] args) {
+        do {
+            double price = MainClass.inputPrice();
+            JOptionPane.showMessageDialog(null, "The item's final price is: $" + MainClass.calculateFinalPrice(price));
+        } while (true); //It'll repeat indefinitely
+    }
+
+
+}
