b1247b824fb7647355d7f0a698be061dc506dafb
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-07 17:34:33 -0500

    11th activity finished

---
+++ src/main/java/dev/j3c/practice11/MainClass.java
@@ -0,0 +1,39 @@
+package dev.j3c.practice11;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainClass {
+
+    private static String inputSentence() {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Ingrese una frase para contar sus caracteres y contar sus vocales: ");
+        String sentence = reader.nextLine();
+        return sentence;
+    }
+
+    public static int getVowelCount(String sentence,char vowel) {
+        int vowelCount = 0;
+        for (int index = 0; index < sentence.length(); index++) {
+            if (sentence.charAt(index) == vowel) {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
+
+    public static void main(String[] args) {
+        String sentence = MainClass.inputSentence();
+        if(sentence.length() > 0) {
+            System.out.println("La longitud de la cadena es, ahora sin espacios es: " + sentence.length());
+            System.out.println("La cantidad de 'a' en la cadena es: " + MainClass.getVowelCount(sentence,'a'));
+            System.out.println("La cantidad de 'a' en la cadena es: " + MainClass.getVowelCount(sentence,'e'));
+            System.out.println("La cantidad de 'a' en la cadena es: " + MainClass.getVowelCount(sentence,'i'));
+            System.out.println("La cantidad de 'a' en la cadena es: " + MainClass.getVowelCount(sentence,'o'));
+            System.out.println("La cantidad de 'a' en la cadena es: " + MainClass.getVowelCount(sentence,'u'));
+        } else {
+            System.out.println("Ingrese una cadena válida.");
+        }
+    }
+
+}
