15cef9eb90f531be6f3f23809712002de255f855
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-07 21:05:11 -0500

    16th activity finished

---
+++ src/main/java/dev/j3c/practice16/Utilidades.java
@@ -0,0 +1,58 @@
+package dev.j3c.practice16;
+
+import javax.swing.*;
+import java.util.InputMismatchException;
+
+public class Utilidades {
+
+    static String inputAndValidateName() {
+        String nombre;
+        do{
+            nombre = JOptionPane.showInputDialog("Ingrese el nombre: ");
+        } while(nombre == null || nombre.length() == 0);
+        return nombre;
+    }
+
+    static int ingresarYValidarEdad() {
+        Integer edad = null;   //Wrapper class (Autoboxing operation)
+        do {
+            try {
+                edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad: "));
+            } catch (NumberFormatException e1) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e1.getMessage());
+            } catch(InputMismatchException e2) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e2.getMessage());
+            }
+        } while(edad == null || edad < 0);
+        return(edad);
+    }
+
+    static char inputAndValidateGender() {
+        Character genero = null;   //Wrapper class (Autoboxing operation)
+        do {
+            try {
+                genero = JOptionPane.showInputDialog("Ingrese el sexo (H para nombre, M para mujer): ").charAt(0);
+            } catch (NumberFormatException e1) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e1.getMessage());
+            } catch(Exception e2) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e2.getMessage());
+            }
+        } while(genero == null || ( genero != 'H' && genero != 'M'));
+        return(genero);
+    }
+
+    static double ingresarYValidarDecimal(String title) {
+        Double pesoOEstatura = null;   //Wrapper class (Autoboxing operation)
+        do {
+            try {
+                pesoOEstatura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese una valor para " + title +": "));
+            } catch (NumberFormatException e1) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e1.getMessage());
+            } catch(InputMismatchException e2) {
+                JOptionPane.showMessageDialog(null,"Exception caught: " + e2.getMessage());
+            }
+        } while(pesoOEstatura == null || pesoOEstatura < 0);
+        return(pesoOEstatura);
+    }
+
+}
